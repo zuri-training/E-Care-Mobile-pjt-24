833db9a9fa1a4189c52c204cda64d6ad4e79f7cf
AKPAKPAN ANIEKAN-ABASI <akpakpan2535@bazeuniversity.edu.ng>
2021-06-18 15:21:42 +0100

    api service setup

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,41 +1,12 @@
-import 'dart:convert';
-import 'package:e_care_mobile/models/patient.dart';
-import 'package:http/http.dart' as http;
+class ApiUrl {
+  static const String liveBaseURL =
+      'https://harvest-rigorous-bambiraptor.glitch.me/app/v1/patient';
 
-//api link
-var url = 'https://harvest-rigorous-bambiraptor.glitch.me/';
+  static const String BaseURL = liveBaseURL;
 
-Future<Patient> registerPatient(
-  //these are the user registration fields
-  String userId,
-  String patientName,
-  String email,
-  String profileImage,
-  String dateofBirth,
-  String password,
-) async {
-  //make a post request, basically trying to send the values to the server
-  final response = await http.post(
-    Uri.parse(
-      '{$url}/api/v1/patient/create',
-    ),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(
-      <String, String>{
-        'userId': userId,
-        'patientName': patientName,
-        'email': email,
-        'profileImage': profileImage,
-        'dateofBirth': dateofBirth,
-        'password': password,
-      },
-    ),
-  );
-  if (response.statusCode == 201) {
-    return Patient.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to create Patient');
-  }
+  static const String register = BaseURL + "/create";
+  static const String login = BaseURL + "/login";
+  static const String forgotPassword = BaseURL + "/patient-forgot-password";
+  static const String resetPassword = BaseURL + "/patient-reset-password";
+  static const String activateAccount = "/activate-patient-account";
 }
