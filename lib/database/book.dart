a2a2781d118844814ea4592e0530ef183050f72d
AKPAKPAN ANIEKAN-ABASI <akpakpan2535@bazeuniversity.edu.ng>
2021-07-08 23:42:01 +0100

    i worked on book appointment screens with firestore

---
+++ lib/database/book.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_care_mobile/screens/profile/utils/user_pref.dart';
+import 'package:e_care_mobile/userData/user.dart';
+import 'package:e_care_mobile/util/AppException.dart';
+import 'package:e_care_mobile/util/shared_preference.dart';
+
+class Book {
+  CollectionReference _db =
+      FirebaseFirestore.instance.collection('bookAppointment');
+
+  User user;
+
+  Future<dynamic> bookAppt(
+    String _id,
+    String date,
+    String doctor,
+    String reason,
+  ) async {
+    try {
+      await _db.doc(_id).set({
+        'Date': date,
+        'Doctor': doctor,
+        'Reason': reason,
+      });
+    } on FirebaseException catch (e) {
+      throw FireStoreException(e.message);
+    }
+  }
+}
